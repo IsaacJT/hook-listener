6d9d4e4f819fd4407cf74392c3b2cf5ebfa40fd7
Isaac True <isaac@is.having.coffee>
2016-07-31 18:27:29 +0200

    Initial test

---
+++ main.go
@@ -0,0 +1,35 @@
+package main;
+
+import (
+  "fmt"
+  "strings"
+  "net/http"
+  "crypto/hmac"
+)
+// checkMAC reports whether messageMAC is a valid HMAC tag for message.
+func checkMAC(message, messageMAC, key string) bool {
+  return checkMAC([]bytes(message), []byte(messageMAC), []byte(key))
+}
+func checkMAC(message, messageMAC, key []byte) bool {
+	mac := hmac.New(sha1.New, key)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	return hmac.Equal(messageMAC, expectedMAC)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  if (r.Method != "POST") {
+    w.WriteHeader(http.StatusMethodNotAllowed)
+    return
+  }
+  if (checkMAC(r.Body, strings.Split(r.Header.Get("X-Hub-Signature"), "=")[1], "test") {
+    w.WriteHeader(http.StatusOK)
+  } else {
+    w.WriteHeader(http.StatusUnauthorized)
+  }
+}
+
+func main() {
+  http.HandleFunc("/", handler)
+  http.ListenAndServe("127.0.0.1:13000", nil)
+}
